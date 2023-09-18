1e29d043560eb68dc12fa8e1afc99769744bc005
dmwilliams <danielmarkwilliams@gmail.com>
2023-09-17 19:32:58 -0500

    Add index.js for /controllers/web

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-const router = require('express').Router(); // This is a function for creating router objects
+const router = require('express').Router(); // This Router function will create the router objects
 const webRoutes = require('./web'); // These are routes to pages that will be visited by the user
 const apiRoutes = require('./api'); // These are routes to the api (for form input)
 
---
+++ controllers/web/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router(); // This Router function will create the router objects
+const login = require('/login.js'); // This is the route to the login page
+const dashboard = require('./dashboard.js'); // This is the route to the dashboard (user's posts)
+const homepage = require('/home.js'); // This is the route to the homepage (all posts)
+
+router.use('/login', login)
+router.use('/dashboard', dashboard)
+router.use('/', homepage);
+
+module.exports = router;
